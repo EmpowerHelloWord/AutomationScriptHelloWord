f28e13cd9bd77195bfee2c76a0e8cc9304c804c5
rsbarros7 <134966901+rsbarros7@users.noreply.github.com>
2023-05-30 15:54:45 -0300

    Adicionar arquivos de projeto.

---
+++ EmpowerHelloWord_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerHelloWord_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerHelloWord_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("D1269F2D-6CAA-4EC7-983A-64E717C27582")]
+[assembly: AssemblyVersion("1.0.0.0")]
